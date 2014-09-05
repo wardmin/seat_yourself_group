41049cfaa5a625d76128214ba47099fabcddf868
callunity <heather.e.armstrong@gmail.com>
2014-09-05 16:47:30 -0400

    Added login for both customers and restaurants.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,6 @@ class Customer < ActiveRecord::Base
 
 	validates :name, presence: true
 	validates :email, presence: true
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :case_sensitive => false, :message => "That email is already taken."
 
 end
