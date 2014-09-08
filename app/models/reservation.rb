38b61771906724990814bc8297198d3c41d88daa
Andrew Smith <smithad15@gmail.com>
2014-09-08 13:54:18 -0400

    Add restaurant booking availability logic

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ActiveRecord::Base
   belongs_to :customer
   belongs_to :restaurant
+
+  validate :availability
+
+  private
+  def availability
+  	unless restaurant.available?(party_size, start_time)
+  		errors[:base] << "Not enough spaces available at that time."
+  	end
+  end
 end
