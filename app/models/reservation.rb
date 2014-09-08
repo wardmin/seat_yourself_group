488815ae00c700a4b01392f2d239ddee956865aa
Ward Minnis <ward@minnis.net>
2014-09-08 14:28:49 -0400

    Changed Reservation model to include time_splitter

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,13 +1,15 @@
 class Reservation < ActiveRecord::Base
-  belongs_to :customer
-  belongs_to :restaurant
+	extend TimeSplitter::Accessors
+  	split_accessor :start_date_time
+ 	belongs_to :customer
+  	belongs_to :restaurant
 
-  validate :availability
+	validate :availability
 
-  private
-  def availability
-  	unless restaurant.available?(party_size, start_time)
-  		errors[:base] << "Not enough spaces available at that time."
-  	end
-  end
+	private
+	  def availability
+	  	unless restaurant.available?(party_size, start_time)
+	  		errors[:base] << "Not enough spaces available at that time."
+	  	end
+	end
 end
