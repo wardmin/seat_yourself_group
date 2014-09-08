907027166fbe51e597a672a373d2dd7d37c83f3f
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-08 15:56:09 -0400

    Example Restaurant added to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.delete_all
+
+Restaurant.create!(name: "Joe's", email: "joe@joe.com", password: "foo", password_confirmation: "foo", address: "123 easy st.", city: "Toronto", province: "Ontario", postal_code: "M4Y 1P4", phone_number: "61778", description: "Good eats")
