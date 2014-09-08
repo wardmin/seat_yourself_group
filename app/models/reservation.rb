4e68d9e45f083688f6e2da406beed3a18aa90996
Andrew Smith <smithad15@gmail.com>
2014-09-08 15:09:54 -0400

    Add Reservation validations and fix availability typo

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,11 +4,15 @@ class Reservation < ActiveRecord::Base
  	belongs_to :customer
   	belongs_to :restaurant
 
+	validates :customer_id, presence: true
+	validates :restaurant_id, presence: true
+	validates :party_size, presence: true, numericality: { only_integer: true }
+	validates :start_date_time, presence: true
 	validate :availability
 
 	private
 	  def availability
-	  	unless restaurant.available?(party_size, start_time)
+	  	unless restaurant.available?(party_size, start_date_time)
 	  		errors[:base] << "Not enough spaces available at that time."
 	  	end
 	end
